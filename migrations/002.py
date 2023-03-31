c63f6b82133328098a58077c8314db4524715980
gil9red <ilya.petrash@inbox.ru>
2023-03-31 14:43:41 +0500

    Рефакторинг

--- migrations/002.py
+++ migrations/002.py
@@ -7,7 +7,7 @@ __author__ = 'ipetrash'
 # SOURCE: http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#schema-migrations
 
 
-from playhouse.migrate import SqliteDatabase, SqliteMigrator, BooleanField, TextField, migrate
+from playhouse.migrate import SqliteDatabase, SqliteMigrator, BooleanField, migrate
 from config import DB_FILE_NAME
 
 
