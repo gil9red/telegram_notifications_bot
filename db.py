0077ef4ff2cb7ee1eaa9cac72e9d85aaa0df9713
gil9red <ilya.petrash@inbox.ru>
2024-07-04 14:42:49 +0500

    Обновлено. В EnumField теперь может быть None

--- db.py
+++ db.py
@@ -56,12 +56,15 @@ class EnumField(CharField):
         super().__init__(*args, **kwargs)
 
         self.choices: Type[enum.Enum] = choices
-        self.max_length = 255
+        self.max_length: int = 255
 
     def db_value(self, value: Any) -> Any:
         return value.value
 
     def python_value(self, value: Any) -> Any:
+        if value is None:
+            return value
+
         type_value_enum = type(list(self.choices)[0].value)
         value_enum = type_value_enum(value)
         return self.choices(value_enum)
