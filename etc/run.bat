2e969b911804adeba4b8336e74b5671f87c37c56
gil9red <ilya.petrash@inbox.ru>
2026-04-15 00:15:47 +0500

    Добавлено. etc/run.bat

---
+++ etc/run.bat
@@ -0,0 +1,3 @@
+chcp 1251
+set PYTHON=C:\Users\ipetrash\PycharmProjects\telegram_notifications_bot\venv\Scripts\python.exe
+%PYTHON% add_notify_use_web.py --name="Jenkins" --message="abc 123" --has-delete-button
